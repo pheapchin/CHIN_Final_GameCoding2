24bd1e1dc76ba659e238abe23243f5d3fdee291b
pheapchin <pheapxchin@gmail.com>
2025-05-06 19:45:45 -0400

    playtest updates

--- Disco_CHIN/Assets/Scripts/PlatformCamera1.cs
+++ Disco_CHIN/Assets/Scripts/PlatformCamera1.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlatformCamera1 : MonoBehaviour
 {
+    public static PlatformCamera1 Instance;
+
     public Transform player;
     public Vector3 offset = new Vector3(0, 2f, -10f);
     public float followSpeed = 5f;
@@ -14,8 +16,21 @@ public class PlatformCamera1 : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+        // Update is called once per frame
+        void LateUpdate()
     {
         if (player == null) return;
 
